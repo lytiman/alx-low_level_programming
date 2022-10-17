cadd3d45f9a5188cf62394fa4e7c675d0dfed6ea
latimaalfa <latialltime2022@gmail.com>
2022-10-17 11:11:36 -0700

    commited

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	float f;
 
 	printf("size of a char:%lu byte(s)", (unsigned  long)sizeof(a));
-	printf("size of a int:%lu byte(s)\n", (usigned long)sizeof(b));
-	printf("size of a long int:%lu byte(s)\n", (usigned long)sizeof(c));
-	printf("size of a long long int:%lu byte(s)\n", (usigned long)sizeof(d));
-	printf("size of a float:%lu byte(s)\n", (usigned long)sizeof(f));
+	printf("size of a int:%lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
