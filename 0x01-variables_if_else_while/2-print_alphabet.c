251673afab7d98d5b067f6b7273d3ff164ca5d14
Letera Mengistu <latomaalfa2020@gmail.com>
2023-02-20 11:52:44 -0800

    this also done 2023

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,16 @@
-#include<stdlib.h>
+#include <stdio.h>
+/**
+ *main - print alpha in lowercase
+ *Return: always 0
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls = 'a'; ls <= 'z'; ls++)
+	{
+		putchar(ls);
+	}
+	putchar ('\n');
+	return (0);
+}
